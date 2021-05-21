a3aaeb84c363e8437a83c853082c35bfdbf53f6d
Pavel_Chupin <pavel.chupin@gmail.com>
2021-05-21 11:25:36 +0700

    полный пример работы с kafka продюсер и консьюмер

--- src/main/java/com/gmail/pavelchupin/kafka_learn/data/dto/UserDto.java
+++ src/main/java/com/gmail/pavelchupin/kafka_learn/data/dto/UserDto.java
@@ -2,12 +2,14 @@ package com.gmail.pavelchupin.kafka_learn.data.dto;
 
 import com.gmail.pavelchupin.kafka_learn.data.Address;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class UserDto {
     private Long age;
     private String name;
